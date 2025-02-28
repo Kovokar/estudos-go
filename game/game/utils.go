0c302b140f585715b060b8d5436a75feae2ae51e
pedro <pkovokar@gmail.com>
2025-02-27 22:55:04 -0300

    colision

--- game/game/utils.go
+++ game/game/utils.go
@@ -9,3 +9,34 @@ type Vector struct {
 	x float64
 	y float64
 }
+
+type Rect struct {
+	x      float64
+	y      float64
+	width  float64
+	height float64
+}
+
+func NewRect(x, y, width, height float64) Rect {
+	return Rect{
+		x:      x,
+		y:      y,
+		width:  width,
+		height: height,
+	}
+}
+
+func (r Rect) Intersects(other Rect) bool {
+	return r.x <= other.maxX() &&
+		other.x <= r.maxX() &&
+		r.y <= other.maxY() &&
+		other.y <= r.maxY()
+}
+
+func (r Rect) maxX() float64 {
+	return r.x + r.width
+}
+
+func (r Rect) maxY() float64 {
+	return r.y + r.height
+}
