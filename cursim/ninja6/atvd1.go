8f96dc0eeb7412f7596c68562c192ba33543c7c8
pedro <pkovokar@gmail.com>
2025-03-05 00:46:28 -0300

    atvd 1 2

--- cursim/ninja6/atvd1.go
+++ cursim/ninja6/atvd1.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-    
+import "fmt"
+
+func main() {
+	fmt.Println(retornaInt())
+	fmt.Println(retornaString())
+}
+
+func retornaInt() int {
+	return 92
+}
+
+func retornaString() string {
+	return "pqp ja uso funcs faz dois anos"
 }
--- cursim/ninja6/atvd2.go
+++ cursim/ninja6/atvd2.go
@@ -1,5 +1,27 @@
 package main
 
-func main(){
-    
+import "fmt"
+
+var list = []int{1, 2, 3, 4}
+
+func main() {
+
+	fmt.Println(spread(1, 2, 3, 4, 5))
+	fmt.Println(sumList(list))
+}
+
+func spread(i ...int) int {
+	tot := 0
+	for _, v := range i {
+		tot += v
+	}
+	return tot
+}
+
+func sumList(list []int) int {
+	sum := 0
+	for _, v := range list {
+		sum += v
+	}
+	return sum
 }
