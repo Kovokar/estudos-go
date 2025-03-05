ea32ff019c79b85d76d79f3a86929e3ae54675d7
pedro <pkovokar@gmail.com>
2025-03-05 01:25:50 -0300

    atvd 3

--- cursim/ninja6/atvd10.go
+++ cursim/ninja6/atvd10.go
@@ -1,5 +1,19 @@
 package main
 
-func main(){
-    
+import "fmt"
+
+func main() {
+	m1 := multiplicador()
+	fmt.Println(m1(2))
+	fmt.Println(m1(3))
+	fmt.Println(m1(4))
+
+}
+
+func multiplicador() func(i int) int {
+	tot := 1
+
+	return func(i int) int {
+		return (tot * i)
+	}
 }
--- cursim/ninja6/atvd9.go
+++ cursim/ninja6/atvd9.go
@@ -1,5 +1,17 @@
 package main
 
-func main(){
-    
+import "fmt"
+
+func callback2(kall func(int) int) {
+	fmt.Println(kall(9))
+}
+
+// Função que será passada como callback
+func elevaQuadrado(x int) int {
+	return x * x
+}
+
+func main() {
+	// Passa a função 'somar' como callback
+	callback2(elevaQuadrado)
 }
