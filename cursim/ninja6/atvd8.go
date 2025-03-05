78427be03188aa067cc3fec1cc23421e94a825a0
pedro <pkovokar@gmail.com>
2025-03-05 01:17:59 -0300

    atvd 3

--- cursim/ninja6/atvd8.go
+++ cursim/ninja6/atvd8.go
@@ -1,5 +1,17 @@
 package main
 
-func main(){
-    
+import "fmt"
+
+func main() {
+	soma := retornaFunc(20)
+	fmt.Println(soma())
+}
+
+func retornaFunc(x int) (f func() int) {
+	soma := x + 10
+
+	somaFunc := func() int {
+		return soma
+	}
+	return somaFunc
 }
