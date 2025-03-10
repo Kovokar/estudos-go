e790b1982bdf25802c2ff3a6f89bf16a1868c979
pedro <pkovokar@gmail.com>
2025-03-10 00:12:32 -0300

    bee

--- bee/simples/2028.go
+++ bee/simples/2028.go
@@ -1,8 +1,43 @@
-for {
-	_,err := fmt.Scanf(&quot;%d\n&quot;,&amp;n)
-  
-	if err == io.EOF {
-	  break
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+func seq(num int, cont int) {
+	lista := []int{0}
+	for i := 1; i <= num; i++ {
+		for j := 0; j < i; j++ {
+			lista = append(lista, i)
+		}
 	}
-	/* your code */
-  }
+	if len(lista) == 1 {
+		fmt.Printf("Caso %d: %d numero\n", cont, len(lista))
+	} else {
+		fmt.Printf("Caso %d: %d numeros\n", cont, len(lista))
+	}
+
+	for i := 0; i < len(lista); i++ {
+		if i == len(lista)-1 {
+			fmt.Println(lista[i]) // Último item sem espaço
+		} else {
+			fmt.Print(lista[i], " ") // Espaço entre os números
+		}
+	}
+}
+
+func main() {
+	cont := 1
+	for {
+		var o int
+		_, err := fmt.Scanf("%d", &o)
+
+		if err == io.EOF {
+			break
+		}
+		seq(o, cont)
+		cont++
+		fmt.Println()
+	}
+}
--- bee/simples/2028.py
+++ bee/simples/2028.py
@@ -1,22 +1,24 @@
-
-def seq(num: int):
-    cont = 1
+def seq(num: int, cont: int):
     lista = [0]
-    for i in range(num+1):
+    for i in range(num + 1):
         for _ in range(i):
-          cont += 1
-          lista.append(i)
+            lista.append(i)
 
-    print(f"Caso {i}: {len(lista)}", "numero" if len(lista) == 1 else "numeros")
-    # for k in lista:
-    #     print(k, end=" ")
-    print("\n")
+    print(f"Caso {cont}: {len(lista)}", "numero" if len(lista) == 1 else "numeros")
+    for i in range(len(lista)):
+        if i == len(lista) - 1:  # Verifica se é o último item
+            print(lista[i])
+        else:
+            print(lista[i], end=" ")
 
 
+
+cont = 1
 while True:
-  try:
-    o = int(input())
-    seq(o)
-  except EOFError:
-    break
-  
+    try:
+        o = int(input())
+        seq(o, cont)
+        print()
+        cont += 1
+    except EOFError:
+        break
