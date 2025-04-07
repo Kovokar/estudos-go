de6877b3ceade834160deb249b53dd0fdfa09456
pedro <pkovokar@gmail.com>
2025-04-07 18:32:25 -0300

    teste

--- bee/simples/1237.go
+++ bee/simples/1237.go
@@ -1,35 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	subString("abcdef", "cdofhij")
+	// fmt.Println(subString("Hey This java is hot", "Java is a new paradigm"))
+
+	exec()
 }
 
 func subString(v1 string, v2 string) int {
+	max := 0
 	for i := 0; i < len(v1); i++ {
 		for k := 0; k < len(v2); k++ {
-			if v1[i] == v2[k] {
-				fmt.Println("chamou")
-				ret := recur(v1, v2, i, k, 0)
-				fmt.Println("wqeqwe", ret)
+			if v1[i] == v2[k] && !verificaEspacoFinal(v1, v2) {
+				ret := recur(v1, v2, i+1, k+1, 1)
+				if ret > max {
+					max = ret
+				}
 			}
 		}
 	}
-	return 0
+	return max
 }
 
 func recur(v1, v2 string, idx1, idx2, cont int) int {
-	// fmt.Println(idx1, idx2)
+	// fmt.Println(idx1, idx2, cont)
+	if idx1 >= len(v1) || idx2 >= len(v2) {
+		return cont
+	}
+
 	if v1[idx1] == v2[idx2] {
-		fmt.Println(string(v1[idx1]), string(v2[idx2]))
-		recur(v1, v2, idx1+1, idx2+1, cont+1)
+		return recur(v1, v2, idx1+1, idx2+1, cont+1)
 	}
-	if cont == 100 {
-		return 99999
-	}
-	fmt.Println("dfwq", cont)
 	return cont
+}
+
+func exec() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		str1, _ := reader.ReadString('\n')
+		str2, _ := reader.ReadString('\n')
+		if str1 == "" || str2 == "" {
+			break
+		}
+		fmt.Println(subString(str1, str2))
+
+	}
+}
+
+func verificaEspacoFinal(palavra1, palavra2 string) bool {
+	return strings.HasSuffix(palavra1, " ") && strings.HasSuffix(palavra2, " ")
 }
